2d5f9340219ad75cc832c427868f91561e28d393
PerezDeborah <PerezDeborahdIc@gmail.com>
2025-01-06 14:08:00 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://iorepemes.monster'
+	const proxyUrl = 'https://kiofdoresa.xyz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
